376787509ac2f5a26d00fa6775497d1bd564d0a0
vinielelopes <vinielelopes@outlook.com>
2022-05-13 22:09:05 -0300

    mudei input pta text area e adicionei variavel alfabeto, pra comer a fazer a cifra de cesar

--- comandos.js
+++ comandos.js
@@ -2,21 +2,19 @@ var cesar = document.getElementById('CC');
 var base64 = document.getElementById('base64');
 var quantDis = document.getElementById('quantDis');
 var codifcado = document.getElementById('codificado');
+var form = document.forms[0];
+var botao = document.getElementById('botao');
+const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 
 cesar.addEventListener('click', function cesarCC(){
     var seletor = document.getElementById('seletor');
     seletor.style.display = 'flex';
 })
+  
 
-var textoNormal = document.getElementById('texto').value;
-var botao = document.getElementById('botao');
-
-botao.addEventListener("click", function codificar(event){
-    event.preventDefault()
-    if(cesar.checked && quantDis === 1){
-        codificado += textoNormal;
-
-    }
+form.addEventListener('submit', function codificar(letra){
+    letra.preventDefault()
+        
 
 })
