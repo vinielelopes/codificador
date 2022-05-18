e005e9169c5bdb754f554671636428b2606e8e81
vinielelopes <vinielelopes@outlook.com>
2022-05-18 18:59:04 -0300

    ainda tentando a cifra de cesar

--- comandos.js
+++ comandos.js
@@ -1,27 +1,59 @@
 var cesar = document.getElementById('CC');
 var base64 = document.getElementById('base64');
-var quantDis = document.getElementById('quantDis');
+var quantDis = document.getElementById('quantDis').value % 26;
 var codifcado = document.getElementById('codificado');
-var botao = document.getElementById('botao');
+var botaoC = document.getElementById('botaoC');
 var texto = document.getElementById("textoUsuario");
 
 const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+ var novaString = " "
 
-
+ // colocar display de contagem distanciamento quando cesar é clicado
 cesar.addEventListener('click', function cesarCC(){
     var seletor = document.getElementById('seletor');
     seletor.style.display = 'flex';
+
 })
-  
 
-botao.addEventListener("click", function codificar(letra){
+ // tirar display de contagem quando base64 for selecionada
+base64.addEventListener('click', function base64(){
+        var seletor = document.getElementById('seletor');
+        seletor.style.display = 'none';
+} )
+
+ // ao clicar botao de codificar, codificar de acordo com qual criptografia escolhida
+botaoC.addEventListener("click", function codificar(letra){
     letra.preventDefault()
-    if(cesar.checked == true ){
+    if(cesar.checked == true){
+        for (let i = 0; i <texto.length; i++){
+            var texLetra = texto[i];
+        }
+        var primeira = alfabeto.indexOf(texLetra);
+        var novaPrim = primeira + quantDis;
+        if (novaPrim > 25){
+            novaPrim = novaPrim - 26
+        } else if(novaPrim < 0) {
+            novaPrim =novaPrim + 26
 
+        } else{
+            novaString += alfabeto[novaPrim];
+            codifcado.innerText += novaString
+        }
+
+    
 
     }else if (base64.checked == true){
         codificado.innerText = `${btoa(texto.value)}`;
     }
-        
+})
 
+ // ao clicar botao de decodificar, decodificar de acordo com a criptografia escolhida
+botaoD.addEventListener("click", function decodificar(letra){
+    letra.preventDefault()
+    if(cesar.checked == true){
+
+
+    }else if (base64.checked == true){
+        codificado.innerText = `${atob(texto.value)}`;
+    }   
 })
