2328e5f3a1778ebfc5e13ce6215a5ae21e4100bc
vinielelopes <vinielelopes@outlook.com>
2022-05-18 16:31:53 -0300

    codificando com base64

--- comandos.js
+++ comandos.js
@@ -2,8 +2,9 @@ var cesar = document.getElementById('CC');
 var base64 = document.getElementById('base64');
 var quantDis = document.getElementById('quantDis');
 var codifcado = document.getElementById('codificado');
-var form = document.forms[0];
 var botao = document.getElementById('botao');
+var texto = document.getElementById("textoUsuario");
+
 const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 
@@ -13,8 +14,14 @@ cesar.addEventListener('click', function cesarCC(){
 })
   
 
-form.addEventListener('submit', function codificar(letra){
+botao.addEventListener("click", function codificar(letra){
     letra.preventDefault()
+    if(cesar.checked == true ){
+
+
+    }else if (base64.checked == true){
+        codificado.innerText = `${btoa(texto.value)}`;
+    }
         
 
 })
