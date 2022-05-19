eb4101f147b6595fcc1da68b77e0159b7ecd7bdd
vinielelopes <vinielelopes@outlook.com>
2022-05-18 23:39:33 -0300

    finalmente conseguindo cifra de cesar, decodificar ta funcionando, mas codificar ainda nao

--- comandos.js
+++ comandos.js
@@ -1,12 +1,9 @@
 var cesar = document.getElementById('CC');
 var base64 = document.getElementById('base64');
-var quantDis = document.getElementById('quantDis').value % 26;
+var quantDis = document.getElementById('quantDis');
 var codifcado = document.getElementById('codificado');
 var botaoC = document.getElementById('botaoC');
 var texto = document.getElementById("textoUsuario");
-
-const alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
- var novaString = " "
 
  // colocar display de contagem distanciamento quando cesar é clicado
 cesar.addEventListener('click', function cesarCC(){
@@ -25,23 +22,7 @@ base64.addEventListener('click', function base64(){
 botaoC.addEventListener("click", function codificar(letra){
     letra.preventDefault()
     if(cesar.checked == true){
-        for (let i = 0; i <texto.length; i++){
-            var texLetra = texto[i];
-        }
-        var primeira = alfabeto.indexOf(texLetra);
-        var novaPrim = primeira + quantDis;
-        if (novaPrim > 25){
-            novaPrim = novaPrim - 26
-        } else if(novaPrim < 0) {
-            novaPrim =novaPrim + 26
-
-        } else{
-            novaString += alfabeto[novaPrim];
-            codifcado.innerText += novaString
-        }
-
-    
-
+        return codC()
     }else if (base64.checked == true){
         codificado.innerText = `${btoa(texto.value)}`;
     }
@@ -51,9 +32,72 @@ botaoC.addEventListener("click", function codificar(letra){
 botaoD.addEventListener("click", function decodificar(letra){
     letra.preventDefault()
     if(cesar.checked == true){
-
-
+        return decodC()
     }else if (base64.checked == true){
         codificado.innerText = `${atob(texto.value)}`;
     }   
 })
+
+
+ // funcao para codificar com a cifra de cesar
+function codC (textoUsuario, distanciamento) {
+    textoUsuario = texto.value;
+    distanciamento = quantDis.value;
+
+    if (distanciamento < 0) {
+      return codificar(textoUsuario, distanciamento + 26);
+    }
+  
+    let resposta = " ";
+    for (let i = 0; i < textoUsuario.length; i++) {
+        let texLetra = textoUsuario[i];
+  
+        if (texLetra.match(/[a-z]/i)) {
+            let code = textoUsuario.charCodeAt(i);
+    
+        if (code >= 65 && code <= 90) {
+            texLetra = String.fromCharCode(((code - 65 + distanciamento) % 26) + 65);
+        }
+    
+        else if (code >= 97 && code <= 122) {
+            texLetra = String.fromCharCode(((code - 97 + distanciamento) % 26) + 97);
+        }
+    }    
+    resposta += texLetra;
+}
+    codificado.innerText += resposta;
+};
+
+ // funcao para decodificar a cifra de cesar
+function decodC (textoUsuario, distanciamento) {
+    textoUsuario = texto.value  
+    distanciamento = quantDis.value;
+
+    if (distanciamento < 0) {
+      return decodificar(textoUsuario, distanciamento + 26);
+    }
+  
+    let resposta = " ";
+    for (let i = 0; i < textoUsuario.length; i++) {
+        let texLetra = textoUsuario[i];
+  
+        if (texLetra.match(/[a-z]/i)) {
+            let code = textoUsuario.charCodeAt(i);
+    
+        if (code >= 65 && code <= 90) {
+            console.log(code)
+            texLetra = (code - 65 - distanciamento < 0)? String.fromCharCode(((code - 65 - distanciamento + 26)%26)+65):String.fromCharCode(((code - 65 - distanciamento)%26)+65)
+        
+        }
+    
+        else if (code >= 97 && code <= 122) {
+            console.log(code)
+            texLetra = String.fromCharCode(((code - 97 - distanciamento + 26) % 26) + 97)
+        }
+    }    
+    resposta += texLetra;
+}
+    codificado.innerText += resposta;
+};
+
+
